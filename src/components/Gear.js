983d17df54eaf2ab36d7b136ca16dbd7b23d3e50
phtn <phtn458@gmail.com>
2017-12-05 08:19:10 -0500

    text size test on mobile

--- src/components/Gear.js
+++ src/components/Gear.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react'
-import { Segment, Image } from 'semantic-ui-react'
+import { Segment, Image, List } from 'semantic-ui-react'
 // import Sword from '../assets/sword.svg'
-// import Helmet from '../assets/helmet.svg'
+import Helmet from '../assets/helmet.svg'
 // import Armor from '../assets/armor.svg'
 // import Boot from '../assets/boot.svg'
 // import Necklace from '../assets/necklace.svg'
 const segment = {
-  background: 'none',
-  border: '0px solid #666',
-  height: window.innerHeight
+  // background: 'none',
+  height: window.innerHeight,
+  padding: 0,
+  border: '1px solid red'
 }
 class Gear extends Component {
 
-  componentWillMount(){
+  getItems(){
     if (this.props.items !== undefined){
-      this.props.items.map(i=> { console.log(i.name)})
+      return this.props.items.map(i=> (
+        <List.Item key={i.key}>
+        <Image avatar src={Helmet} />
+        <List.Content>
+          <List.Header>{i.name}</List.Header>
+        </List.Content>
+        <List.Content>
+          <List>{this.getAttr(i.attr)}</List>
+        </List.Content>
+      </List.Item>
+      ))
+    }
+  }
+  getAttr(attr){
+    if (attr !== undefined){
+      return attr.map(a=> (
+        <List.Item key={a.key}>
+          <List.Content>
+            {a.name}
+          </List.Content>
+          <List.Content floated='right'>
+            {a.key}
+          </List.Content>
+        </List.Item>
+      ))
     }
   }
   render(){
     return (
       <div>
         <Segment style={segment}>
-          
-           {/* <ol>{this.getItems()}</ol> */}
+          <List selection verticalAlign='middle'>
+            {this.getItems()}
+          </List>
         </Segment>
       </div>
     )
